5ce7029171613c6779a80d7386808c63aaa696a2
CryoliteZ <cryolite@ntu.im>
2017-03-22 22:09:30 +0800

    before deadline

--- hw1/findIndex.py
+++ hw1/findIndex.py
@@ -1,7 +1,7 @@
 import csv
 
 def loadTrainData(dataNum):
-	f = open('data1/training'+ dataNum +'.csv', 'r')
+	f = open('data3/training'+ str(dataNum) +'.csv', 'r')
 	for row in csv.reader(f):
 		results = list(map(float, row))
 		data.append(results)
@@ -9,13 +9,35 @@ def loadTrainData(dataNum):
 	main  = open('PMdata.csv', 'r')
 	for row in csv.reader(main):
 		if(row[1] == "PM2.5"):
-			row = row[2:]
+			row = list(map(float,row[2:]))
+			if(-1 not in row):
+				mData.append(row)
+
+
+def findIndex():
+	day = 0
+	for s in data:	
+		index = [(i, i+len(s)) for i in range(len(mData[day])) if mData[day][i:i+len(s)] == s]		
+		if(len(index) == 0):
+			day = day + 1
+			index = [(i, i+len(s)) for i in range(len(mData[day])) if mData[day][i:i+len(s)] == s]
+		dataIndex.append((day,index[0][0]))
 
 
 data = []
+mData = []
+dataIndex = []
+
+loadTrainData(14)
+findIndex()
+
 
 
 a = [1,4,5,6,7,8,4]
 b = [4,5,6]
+
+f = open('index.csv','w')
+for index in dataIndex:
+	f.write(str(index[0]) + ',' + str(index[1]) + '\n')	
 
 
---
+++ hw1/hw1.py
@@ -0,0 +1,156 @@
+import csv, math
+import time
+
+def loadData(varNum, para, num):
+	if(para == 1):
+		f = open('data' + varNum + '/training'+ num +'.csv', 'r')
+	else:
+		f =  open('data' + varNum + '/all'+ num +'.csv', 'r')
+	for row in csv.reader(f):
+		rowLen = len(row)
+		break
+	tmpValue = [0.0] * rowLen
+	tmpY = 0.0	
+	
+	for row in csv.reader(f):
+		row = list(map(float, row))
+		y_value = row[9]
+		del row[9]
+		s = (y_value, row)
+		
+		tmpValue = [x + y for x, y in zip(tmpValue, s[1])]
+		tmpY = tmpY +  s[0]
+		trainSet.append(s)
+
+	tmpValue[:] = [(x / len(trainSet) ) for x in tmpValue]
+	avgY = tmpY / len(trainSet)	
+	avgPara = avgY/sum(tmpValue)
+
+	f.close()
+
+	f = open('data' + varNum + '/valid'+ num +'.csv', 'r')
+	for row in csv.reader(f):
+		y_value = row[9]
+		del row[9]
+		row = list(map(float, row))
+		validSet.append((float(y_value), row))
+	f.close()
+
+	return avgPara
+
+
+
+def gradientDescent(iteration):	
+	minError = 99999
+	b = 1 # initial b
+	w = [initPara] * len(trainSet[0][1]) # initial w1 ... w9
+	lr = 0.1 # learning rate
+
+	b_lr = 0.0
+	w_lr = [0.0] * len(trainSet[0][1])	
+
+	# Store initial values for plotting.
+	b_history.append(b)
+	w_history.append(w)
+
+	# Iterations
+	for it in range(iteration):
+	    
+	    b_grad = 0.0
+	    w_grad = [0.0]* 18
+	    for n in range(len(trainSet)):
+	       	b_grad = b_grad  - 2.0*(trainSet[n][0] - b - sum([a*b for a,b in zip(w,trainSet[n][1])]) )*1.0
+	       	for i in range(0,9): 
+	        	w_grad[i] = w_grad[i]  - 2.0*(trainSet[n][0] - b - sum([a*b for a,b in zip(w,trainSet[n][1])]))*trainSet[n][1][i]
+	    
+	    b_lr = b_lr + b_grad**2
+	    # Update parameters.
+	    b = b - lr/math.sqrt(b_lr) * b_grad 
+	    for i in range(0,9):
+	   		w_lr[i] = w_lr[i] + w_grad[i]**2
+	   		w[i] = w[i] - lr/math.sqrt(w_lr[i]) * w_grad[i]   		
+	    
+	    # Store parameters 
+	    b_history.append(b)
+	    w_history.append(w)			
+
+def errorChecking(optModel, validSet):
+	SE = 0
+	for vs in validSet:
+		SE = SE + (vs[0] - ( optModel[0] + sum([a*b for a,b in zip(vs[1],optModel[1])])) ) ** 2
+	MRSE = 	math.sqrt(SE/len(validSet))
+	return(MRSE)
+
+
+def filterData():
+	f = open('test_X.csv', 'r')	
+	for row in csv.reader(f):				
+		if(row[1] == "PM2.5"):	
+			results = list(map(float, row[2:]))
+			data.append(results)	
+	f.close()
+
+def sub(model):
+	b = model[0]
+	model = model[1:]
+	for i in range(0,len(data)):	
+		y[i] = b + sum([a*b for a,b in zip(model,data[i])])
+		if(y[i] <= 0):
+			y[i] = 0
+
+	with open("result.csv", "w", newline='') as mFile:
+		writer = csv.writer(mFile)
+		writer.writerow(["id","value"])
+		for i in range(0, len(data)):
+			mFile.write("id_" + str(i) + ",")
+			mFile.write(str(y[i]))
+			mFile.write("\n")
+
+b_history = []
+w_history = []
+trainSet = []
+validSet = []
+data = []
+y = [0] * 240
+
+start_time = time.time()
+
+
+varNum = "3"
+num = "13"
+mode = "2"
+initPara = loadData(varNum, int(mode), num) # 1 for first time training, 2 for load all
+iteration = 5000
+gradientDescent(iteration)
+
+minError = 9999
+optModel = None
+it = 0
+for i in range(iteration):
+	m = (b_history[i],w_history[i])
+	err = errorChecking(m,validSet)
+	if(err < minError):
+		minError = err
+		optModel = m
+		it = i
+MRSE = minError
+
+
+model = [optModel[0]] + optModel[1]
+	
+filterData()
+sub(model)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+# errorChecking(optModel)
+
+
+
+
+
+
+
+
