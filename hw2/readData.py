4106e8886f65766fa68accaebf4dbb2da34c506e
CryoliteZ <cryolite@ntu.im>
2017-03-31 14:34:22 +0800

    after baseline

--- hw2/readData.py
+++ hw2/readData.py
@@ -2,6 +2,8 @@ import csv, random, math
 from operator import add
 import numpy as np
 from numpy.linalg import inv
+import matplotlib.pyplot as plt
+
 
 class Man():
 	def __init__(self):
@@ -19,8 +21,8 @@ class Man():
 		self.country = []
 		self.flag = 0
 
-def loadData(data):
-	f = open('X_train.csv', 'r')
+def loadData(data, d, flag):
+	f = open(d, 'r')
 	index = 0
 	for row in csv.reader(f):
 		if (index == 0):
@@ -36,8 +38,8 @@ def loadData(data):
 		m.capital_gain = [row[3]]
 		m.capital_loss = [row[4]]
 		m.hours_per_week = [row[5]]
-		m.eduStatus = row[6:15]
-		m.workClass = row[15:31]
+		m.workClass = row[6:15]
+		m.eduStatus = row[15:31]
 		m.marryStatus = row[31:38]
 		m.occupation = row[38:53]
 		m.relation = row[53:59]
@@ -46,18 +48,20 @@ def loadData(data):
 		data.append(m)
 		index = index + 1
 	f.close()
-	f = open('Y_train.csv','r')
-	index = 0
-	for row in csv.reader(f):
-		data[index].flag = int(row[0])
-		index = index + 1
-	f.close()
+
+	if(flag == 1):
+		f = open('Y_train.csv','r')
+		index = 0
+		for row in csv.reader(f):
+			data[index].flag = int(row[0])
+			index = index + 1
+		f.close()
 	
 	return data
 
 def splitData(data):
 	dataLen = len(data)
-	random.seed(50)
+	random.seed(7122)
 	random.shuffle(data)
 	train_data = data[:math.floor(dataLen*0.7)]
 	valid_data = data[math.floor(dataLen*0.7):]
@@ -68,11 +72,17 @@ def gaussianDistribution(x,mean,sigma):
 	# print(x)
 	# print(mean)
 	pi = math.pi
-	
+	w = np.array([x - mean])
+	print(w)
+	print(w.transpose())
+	print(np.matrix(sigma))
+	print(inv(np.matrix(sigma)))
 	para1 = 1 / ((2 * pi) ** (1/2))
 	para2 = 1 / ((np.linalg.det(sigma)) ** (1/2))
-	para3 = math.exp((-1/2)*  ( np.dot(np.dot((x - mean),inv(sigma) ),  (x-mean).transpose())))
-	ans = para1*para2*para3
+	r = np.matmul(w,inv(sigma))
+	para3 = math.exp(np.matmul(r, w.transpose()))
+
+	ans = para1 * para2 * para3
 	print(ans)
 	return ans
 
@@ -84,18 +94,21 @@ def maxLikelihood(x_vector, flag):
 	# mean_vector[:] = [(x / len(x_vector) ) for x in mean_vector]
 
 	# compute mean vector
+	# print("x_vector:")
+	# print(x_vector)
 	length = len(x_vector)
-	mean_vector = np.array([0] * len(x_vector[0]))
+	mean_vector = np.zeros(len(x_vector[0]))
 	for i in range(length):
 		mean_vector = mean_vector + x_vector[i]
 	mean_vector = mean_vector / length
+	# print("mean_vector:")
 
 	# compute covariance vector
 	sigma = np.zeros((len(x_vector[0]),len(x_vector[0])))
-	for i in range(length):
-		x = np.array([(x_vector[i] - mean_vector)])
-		sigma = sigma + np.dot(x.transpose(), x)
-	sigma = sigma/length
+	# for i in range(length):
+	# 	x = np.array([(x_vector[i] - mean_vector)])
+	# 	sigma = sigma + np.dot(x.transpose(), x)
+	# sigma = sigma/length
 
 	
 	# flag 0 for class 0, 1 for class 1
@@ -112,38 +125,184 @@ def classification(data):
 
 def selectAttr(cData):
 	# select the attr. we want to take into consideration
-	x_vector = [[],[]]
-	for i in range(2):		
-		for j in range(len(cData[i])):
-			x_vector[i].append(np.concatenate((cData[i][j].eduStatus,cData[i][j].marryStatus),0))
+	if(len(cData) == 2):
+		x_vector = [[],[]]
+		for i in range(2):		
+			for j in range(len(cData[i])):
+				x_vector[i].append(np.concatenate((cData[i][j].workClass,cData[i][j].marryStatus, cData[i][j].occupation, cData[i][j].country, cData[i][j].eduStatus),0))
+	else:
+		x_vector = []
+		for i in range(len(cData)):
+				x_vector.append(np.concatenate((cData[i].workClass,cData[i].marryStatus, cData[i].occupation, cData[i].country, cData[i].eduStatus),0))
+
+
 
 	return x_vector
+
+def bernoulli(x,mean):
+	# At C1
+	# workclass 0:9
+	p1 = 0
+	for i in range(0,9):
+		p1 = p1 + x[i] * mean[i]
+	# marry 9:16
+	p2 = 0
+	for i in range(9,16):
+		p2 = p2 + x[i] * mean[i]
+	# # race 16:21
+	# p3 = 0
+	# for i in range(16,21):
+	# 	p3 = p3 + x[i] * mean[i]
+	# occupation 21:36
+	p4 = 0
+	for i in range(16,31):
+		p4 = p4 + x[i] * mean[i]
+	# country 42 elements
+	p5 = 0
+	for i in range(31,73):
+		p5 = p5 + x[i] * mean[i]
+	p6 = 0 
+	# education status 
+	for i in range(73,89):
+		p6 = p6 + x[i] * mean[i]
+	
+	return p1 * p2  * p4 * p5 * p6
+
+def prob(x, x_vector, mean0, mean1, feature):
+	P = 0
+	P_C0 = len(x_vector[0]) / (len(x_vector[0]) + len(x_vector[1]))
+	P_C1 = len(x_vector[1]) / (len(x_vector[0]) + len(x_vector[1]))
+	if(feature == 0):
+		P = bernoulli(x,mean0) * P_C0 / (bernoulli(x,mean0) * P_C0 + bernoulli(x,mean1) * P_C1)		
+	else:
+		P = bernoulli(x,mean1) * P_C1 / (bernoulli(x,mean0) * P_C0 + bernoulli(x,mean1) * P_C1)	
+	return P
 
 
 data = []
 train_data = []
 valid_data = []
 # load data
-data = loadData(data)
+data = loadData(data, 'X_train.csv', 1)
+
 # split train/ valid set
 (train_data, valid_data) = splitData(data)
 # classificate train set
 cData = classification(train_data)
 # select the desired attr.
-x_vector = selectAttr(cData)
-print(len(x_vector[0]))
+x_main_vector = selectAttr(cData)
+# print(len(x_vector[0]))
+
 
 # for testing
-x = [np.array([1,2,3,6]),np.array([5,6,8,8]),np.array([4,2,1,6])]
+# x = [np.array([1,0,0]),np.array([0,1,0]),np.array([0,0,1]),np.array([0,0,1]),np.array([0,0,1]),np.array([0,0,1])]
 
-# compute opt. mean, cov matrix
-(mean_vector, sigma, flag) = maxLikelihood(x_vector[0],0)
+# compute opt. mean, cov matrix of class 0
+(mean_vector_0, sigma, flag) = maxLikelihood(x_main_vector[0],0)
+(mean_vector_1, sigma, flag) = maxLikelihood(x_main_vector[1],1)
+
+# select the desired attr.
+x_vector_valid = selectAttr(data)
+
+
+# # load test data
+test_data = []
+test_data = loadData(test_data, 'X_test.csv', 0)
+# print(len(test_data))
+# select the desired attr.
+x_test_vector = selectAttr(test_data)
+
+
+y_pred = []
+for i in range(len(x_test_vector)):
+	if(prob(x_test_vector[i], x_main_vector, mean_vector_0, mean_vector_1,1) > 0.5):
+		y_pred.append(1)		
+	else:
+		y_pred.append(0)
+
+with open("result4.csv", "w", newline='') as mFile:
+	writer = csv.writer(mFile)
+	writer.writerow(["id","label"])
+	for i in range(0, len(x_test_vector)):
+		mFile.write(str(i+1) + ",")
+		mFile.write(str(y_pred[i]))
+		mFile.write("\n")
+
+# #test accuracy
+# p = 0
+# for i in range(len(x_test_vector)):
+# 	if(y_pred[i] == data[i].flag):
+# 		p = p + 1
+# print(p)
+# print(len(x_test_vector))
+# print( p / len(x_test_vector))
+
+
+
+
+# print("Cov")
+# print(sigma)
+# print("Det(Cov)")
+# print(np.linalg.det(sigma))
+# z = gaussianDistribution(np.array([0,1,0]), mean_vector, sigma)
 
 
 # following went wrong
-for i in range(len(x_vector[0])):
-	gaussianDistribution(x_vector[0][i], mean_vector, sigma)
+# for i in range(len(x_vector[0])):
+# 	gaussianDistribution(x_vector[0][i], mean_vector, sigma)
 
 
-# z = gaussianDistribution(np.array([80,70]),np.array([75, 71]),np.array([[874,327],[327,929]]))
-# print(z)
+# y = scipy.stats.multivariate_normal.pdf(x, mean=mean_vector, cov=sigma)
+
+# z = gaussianDistribution(np.array([75,71]),np.array([75, 71]),np.array([[874,327],[327,929]]))
+
+
+# a = np.matrix(sigma)
+# b = inv(sigma)
+# print(np.linalg.det(sigma))
+# print("sigma")
+# print(a)
+
+# print("singma inverse")
+# print(b)
+# print("sigma * singma inverse")
+# print(np.matmul(a,b))
+
+# P_C0 = len(x_vector[0]) / (len(x_vector[0]) + len(x_vector[1]))
+# print(P_C0)
+# P_C1 = len(x_vector[1]) / (len(x_vector[0]) + len(x_vector[1]))
+# print(P_C1)
+
+# print(mean_vector_0)
+# print(mean_vector_1)
+# print("is 0, 0 prob")
+# p = 0
+# for i in range(len(x_vector[0])):
+# 	if(prob(x_vector[0][i], x_vector, mean_vector_0, mean_vector_1,0) > 0.5):
+# 		p = p + 1
+# p = p / len(x_vector[0])
+# print(p)
+
+# print("is 1, 0 prob")
+# p = 0
+# for i in range(len(x_vector[1])):
+# 	if(prob(x_vector[1][i], x_vector, mean_vector_0, mean_vector_1,0) > 0.5):
+# 		p = p + 1
+# p = p / len(x_vector[1])
+# print(p)
+
+# print("is 0, 1 prob")
+# p = 0
+# for i in range(len(x_vector[0])):
+# 	if(prob(x_vector[0][i], x_vector, mean_vector_0, mean_vector_1,1) > 0.5):
+# 		p = p + 1
+# p = p / len(x_vector[0])
+# print(p)
+
+# print("is 1, 1 prob")
+# p = 0
+# for i in range(len(x_vector[1])):
+# 	if(prob(x_vector[1][i], x_vector, mean_vector_0, mean_vector_1,1) > 0.5):
+# 		p = p + 1
+# p = p / len(x_vector[1])
+# print(p)
---
+++ hw2/sub.py
@@ -0,0 +1,101 @@
+import csv, random, math
+from operator import add
+import time
+
+model = [-1.6088496727830721,-1.541155112642145,-1.1057176916750466,-2.1632065845172597,-1.8609494266495183,-2.095230006126708,-1.7957492936293054,-0.24521607225114378,-0.26766261675284775,0.33997622257442023,1.372518147677843,-0.793567297449567,0.6855452197990481,-12.270391815537897,1.18460899089318,-0.461227316631064,0.0908236158545571,-0.598187333773533,-4.899972381014815,-0.40325025690246297,-0.22064481873974348,-0.8776958846737789,-0.7257000667519868,-6.808094368850253,-0.8785338213878366,-1.6208222520491253,1.2607614985478617,0.5446548849899325,-1.5925611915423532,-2.1328767546576564,-1.7484609613148376,-1.6527833388989792,-0.6609457438190753,-1.8547415350589613,-0.6487605772460965,0.08981861575041031,-1.745382381268321,-1.4005572762317273,-0.9854762950094985,-1.4941921150027886,-4.838639482606251,-0.17051625776434862,-0.12284695031752675,-0.4303818113310265,-0.046734398947950534,-0.8207608994025183,-0.8959458531578055,1.8459651141232445,2.8694457351285583,29.98192711390269,2.8365762547636617,0.6168124436774903,-0.3578935805028152,-1.3694882606112,-2.8463592764827075,-0.37176317989665575,-2.43159724492761,-1.0333231287020779,-1.2587760152990595,-0.37645646764358376,-0.054739445715681054,-0.3029188665674062,-1.705794765136514,-0.9042607965339186,-0.8122771521137988,-5.712087438864613,-1.692478335473457,-0.6701903735298327,-0.8029101142942069,-1.0865482809686224,-0.691877022115834,-0.21109876787223913,0.10192433130029796,-0.689502530513102,-0.30937338283031557,-1.1884465796588646,-1.2011871662085294,-1.3452676149452512,-6.4026069068629745,-1.4915823707409364,-0.35396338632382973,-0.7447781698906126,-0.6644594518731008,-0.9564979675068966,-0.4560433940088128,-1.7051884793337655,-0.6429203243633148,-1.1956574692288304,-1.0128187657134644,-0.5226748744064894,-1.8807961464067124,-0.06326763110738655,-0.882933228463128,-1.1421739716556838,-0.5044488899928521,-0.6830079720179648,-0.9649920233747619,-0.5429182861653814,0.13468313848160762]
+class Man():
+	def __init__(self):
+		self.age = []
+		self.fnlwgt = []
+		self.sex = []
+		self.capital_gain = []
+		self.capital_loss = []
+		self.workClass = []
+		self.eduStatus = []
+		self.marryStatus = []
+		self.occupation = []
+		self.relation = []
+		self.race = [] 
+		self.country = []
+		self.flag = 0
+
+def f_wb(x_n, w, b):
+	z = sum([a*b for a,b in zip(w,x_n)]) + b
+	ans = 1/(1+math.exp(-1 * z))
+	return ans
+
+def loadData(data, d, flag):
+	f = open(d, 'r')
+	index = 0
+	for row in csv.reader(f):
+		if (index == 0):
+			attr_header = row
+			index = index + 1
+			continue
+		row = list(map(float, row))		
+		m = Man()
+		m.age = [row[0]]
+		m.fnlwgt = [row[1]]
+		m.sex = [row[2]]
+		m.capital_gain = [row[3]]
+		m.capital_loss = [row[4]]
+		m.hours_per_week = [row[5]]
+		m.workClass = row[6:15]
+		m.eduStatus = row[15:31]
+		m.marryStatus = row[31:38]
+		m.occupation = row[38:53]
+		m.relation = row[53:59]
+		m.race = row[59:64]
+		m.country = row[64:]
+		data.append(m)
+		index = index + 1
+	f.close()
+
+	if(flag == 1):
+		f = open('Y_train.csv','r')
+		index = 0
+		for row in csv.reader(f):
+			data[index].flag = int(row[0])
+			index = index + 1
+		f.close()
+	
+	return data
+
+def selectAttr(cData):
+	# select the attr. we want to take into consideration
+	# if(len(cData) == 2):
+	# 	x_vector = [[],[]]
+	# 	for i in range(2):		
+	# 		for j in range(len(cData[i])):
+	# 			x_vector[i].append(np.concatenate((cData[i].eduStatus + cData[i].workClass + cData[i].marryStatus + cData[i].occupation),0))
+	# else:
+	x_vector = []
+	y_vector = []
+	for i in range(len(cData)):
+		y_vector.append(cData[i].flag)
+		x_vector.append(cData[i].eduStatus + cData[i].workClass + cData[i].marryStatus + cData[i].age + cData[i].hours_per_week + cData[i].capital_gain + cData[i].capital_loss + cData[i].sex)				
+	return (x_vector, y_vector)
+
+test_data = []
+
+test_data = loadData(test_data, 'X_test_norm.csv', 0)
+print(test_data[0].marryStatus)
+y_pred = [0] * len(test_data)
+(x_test_vector, yxx) = selectAttr(test_data)
+
+print(len(x_test_vector[0]))
+print(len(model))
+
+for i in range(len(x_test_vector)):
+	if(f_wb(x_test_vector[i], model, -0.5686802931902314) >= 0.5):
+		y_pred[i] = 1
+	else:
+		y_pred[i] = 0
+
+with open("result66.csv", "w", newline='') as mFile:
+	writer = csv.writer(mFile)
+	writer.writerow(["id","label"])
+	for i in range(0, len(x_test_vector)):
+		mFile.write(str(i+1) + ",")
+		mFile.write(str(y_pred[i]))
+		mFile.write("\n")
